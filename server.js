e94b99287d132f6b27d79b3765c7d032f374acb6
Ed Millington <e.r.millington@dunelm.org.uk>
2016-10-03 17:01:47 +1300

    First commit: deploying empty mongo db to heroku

---
+++ server.js
@@ -0,0 +1,81 @@
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
+
+var USERS_COLLECTION = "users";
+
+var app = express();
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
+
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");
+
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
+
+// USERS API ROUTES BELOW
+
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
+
+/*  "/users"
+ *    GET: finds all users
+ *    POST: creates a new user
+ */
+
+app.get("/users", function(req, res) {
+});
+
+app.post("/users", function(req, res) {
+  var newUser = req.body;
+  newUser.createDate = new Date();
+
+  if (!(req.body.firstName || req.body.lastName)) {
+    handleError(res, "Invalid user input", "Must provide a first or last name.", 400);
+  }
+
+  db.collection(USERS_COLLECTION).insertOne(newUser, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to create new user.");
+    } else {
+      res.status(201).json(doc.ops[0]);
+    }
+  });
+});
+
+
+/*  "/users/:id"
+ *    GET: find user by id
+ *    PUT: update user by id
+ *    DELETE: deletes user by id
+ */
+
+app.get("/users/:id", function(req, res) {
+});
+
+app.put("/users/:id", function(req, res) {
+});
+
+app.delete("/users/:id", function(req, res) {
+});
