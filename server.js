61a43a8f4da9ef146a4cae87fe1d634101427319
Ed Millington <e.r.millington@dunelm.org.uk>
2016-10-03 18:39:03 +1300

    Added methods to 'get' 'put' and 'delete' users by ID

--- server.js
+++ server.js
@@ -45,6 +45,13 @@ function handleError(res, reason, message, code) {
  */
 
 app.get("/users", function(req, res) {
+  db.collection(USERS_COLLECTION).find({}).toArray(function(err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get users.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
 });
 
 app.post("/users", function(req, res) {
@@ -64,7 +71,6 @@ app.post("/users", function(req, res) {
   });
 });
 
-
 /*  "/users/:id"
  *    GET: find user by id
  *    PUT: update user by id
@@ -72,10 +78,34 @@ app.post("/users", function(req, res) {
  */
 
 app.get("/users/:id", function(req, res) {
+  db.collection(USERS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to get user");
+    } else {
+      res.status(200).json(doc);
+    }
+  });
 });
 
 app.put("/users/:id", function(req, res) {
+  var updateDoc = req.body;
+  delete updateDoc._id;
+
+  db.collection(USERS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to update user");
+    } else {
+      res.status(204).end();
+    }
+  });
 });
 
 app.delete("/users/:id", function(req, res) {
+  db.collection(USERS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+    if (err) {
+      handleError(res, err.message, "Failed to delete user");
+    } else {
+      res.status(204).end();
+    }
+  });
 });
